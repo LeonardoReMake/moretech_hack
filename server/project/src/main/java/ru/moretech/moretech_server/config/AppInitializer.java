bda419e36a064f80411b138ce510a06548dc0f1e
Leonid Smirnov <remakeleonardo@gmail.com>
2020-10-04 23:26:26 +0300

    Added simple configuration. Added dependencies.

---
+++ server/project/src/main/java/ru/moretech/moretech_server/config/AppInitializer.java
@@ -0,0 +1,24 @@
+package ru.moretech.moretech_server.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    private static final Logger LOG = LoggerFactory.getLogger(AppInitializer.class);
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
