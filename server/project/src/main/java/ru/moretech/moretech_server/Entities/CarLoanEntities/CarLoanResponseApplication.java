262dcd90b50eddef1ed33ca105075ae7f7589b30
alex <skachkov.ap@phystech.edu>
2020-10-10 00:56:48 +0300

    Added CarLoanEntities

--- server/project/src/main/java/ru/moretech/moretech_server/Entities/CarLoanEntities/CarLoanResponseApplication.java
+++ server/project/src/main/java/ru/moretech/moretech_server/Entities/CarLoanEntities/CarLoanResponseApplication.java
@@ -2,5 +2,4 @@ package ru.moretech.moretech_server.Entities.CarLoanEntities;
 
 public class CarLoanResponseApplication {
     private int VTB_client_ID;
-
 }
---
+++ server/project/src/main/java/ru/moretech/moretech_server/Entities/CarLoanEntities/CarLoanResponseDecision.java
@@ -0,0 +1,9 @@
+package ru.moretech.moretech_server.Entities.CarLoanEntities;
+
+public class CarLoanResponseDecision {
+    private String application_status;
+    private String comment;
+    private String decision_date;
+    private String decision_end_date;
+    private int Payment;
+}
