4f0566b766b6ae3da6931d55d2df4bf6c4590ac6
alex <skachkov.ap@phystech.edu>
2020-10-10 00:54:43 +0300

    Added CarLoanEntities

--- server/project/src/main/java/ru/moretech/moretech_server/Entities/RecognitionEntities/CarResponse.java
+++ server/project/src/main/java/ru/moretech/moretech_server/Entities/RecognitionEntities/CarResponse.java
@@ -8,4 +8,12 @@ import java.util.Map;
 public class CarResponse {
     @JsonProperty
     private Map<String, Double> probabilities;
+
+    public Map<String, Double> getProbabilities() {
+        return probabilities;
+    }
+
+    public void setProbabilities(Map<String, Double> probabilities) {
+        this.probabilities = probabilities;
+    }
 }
